1a5caedbdff21cb80ed5c20300f7ae653893aebe
upndr <104949293+upndr@users.noreply.github.com>
2025-04-25 23:18:19 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -2,6 +2,6 @@ resource "aws_lb" "web_alb" {
   name               = "web-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = ["subnet-xxxxx", "subnet-yyyyy"] # Add your subnets
+  subnets            = ["subnet-xxxxx", "subnet-yyyyy"]
   security_groups    = [aws_security_group.web_sg.id]
 }
